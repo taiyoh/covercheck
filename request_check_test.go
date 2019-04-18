eca560ff12b29a4f0fe83a2f9a78c1bb47380855
taiyoh <sun.basix@gmail.com>
2019-04-18 09:05:34 +0900

    no need to test for wrong request method

--- request_check_test.go
+++ request_check_test.go
@@ -13,11 +13,6 @@ import (
 func TestRequestChceckGET(t *testing.T) {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/foo", func(w http.ResponseWriter, r *http.Request) {
-		if r.Method != http.MethodGet {
-			w.WriteHeader(http.StatusMethodNotAllowed)
-			w.Write([]byte("NG"))
-			return
-		}
 		if r.URL.RawQuery != "hoge=fuga" {
 			w.WriteHeader(http.StatusBadRequest)
 			w.Write([]byte("NG"))
@@ -30,56 +25,35 @@ func TestRequestChceckGET(t *testing.T) {
 	testserver := httptest.NewServer(mux)
 	defer testserver.Close()
 
-	t.Run("wrong request method", func(t *testing.T) {
-		u, _ := url.Parse(testserver.URL + "/foo?hoge=fuga")
-		headers := http.Header{
-			"Content-Type": []string{
-				"application/x-www-form-urlencoded",
-			},
-		}
-		c := covercheck.NewRequestCheckerPOST(u, headers, []byte("aaa=iii"))
+	for _, tt := range []struct {
+		label       string
+		query       string
+		expectedErr bool
+	}{
+		{
+			"wrong query",
+			"hoge=fugo",
+			true,
+		},
+		{
+			"valid request",
+			"hoge=fuga",
+			false,
+		},
+	} {
+		u, _ := url.Parse(testserver.URL + "/foo?" + tt.query)
+		c := covercheck.NewRequestCheckerGET(u, nil)
 		checker := c.Checker()
-		if err := checker(context.Background()); err == nil {
-			t.Error("POST request should return error")
+		actual := checker(context.Background()) != nil
+		if actual != tt.expectedErr {
+			t.Errorf("[%s] expected:%v, actual:%v", tt.label, tt.expectedErr, actual)
 		}
-	})
-
-	t.Run("GET request", func(t *testing.T) {
-		for _, tt := range []struct {
-			label       string
-			query       string
-			expectedErr bool
-		}{
-			{
-				"wrong query",
-				"hoge=fugo",
-				true,
-			},
-			{
-				"valid request",
-				"hoge=fuga",
-				false,
-			},
-		} {
-			u, _ := url.Parse(testserver.URL + "/foo?" + tt.query)
-			c := covercheck.NewRequestCheckerGET(u, nil)
-			checker := c.Checker()
-			actual := checker(context.Background()) != nil
-			if actual != tt.expectedErr {
-				t.Errorf("[%s] expected:%v, actual:%v", tt.label, tt.expectedErr, actual)
-			}
-		}
-	})
+	}
 }
 
 func TestRequestChceckPOST(t *testing.T) {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/foo", func(w http.ResponseWriter, r *http.Request) {
-		if r.Method != http.MethodPost {
-			w.WriteHeader(http.StatusMethodNotAllowed)
-			w.Write([]byte("NG"))
-			return
-		}
 		if r.URL.RawQuery != "hoge=fuga" {
 			w.WriteHeader(http.StatusBadRequest)
 			w.Write([]byte("NG"))
@@ -99,62 +73,50 @@ func TestRequestChceckPOST(t *testing.T) {
 	testserver := httptest.NewServer(mux)
 	defer testserver.Close()
 
-	t.Run("wrong request method", func(t *testing.T) {
-		u, _ := url.Parse(testserver.URL + "/foo?hoge=fuga")
-		c := covercheck.NewRequestCheckerGET(u, nil)
+	headers := http.Header{
+		"Content-Type": []string{
+			"application/x-www-form-urlencoded",
+		},
+	}
+	for _, tt := range []struct {
+		label       string
+		query       string
+		form        string
+		expectedErr bool
+	}{
+		{
+			"wrong query",
+			"hoge=fugo",
+			"foo=bar&aaa=iii",
+			true,
+		},
+		{
+			"wrong post body",
+			"hoge=fuga",
+			"aaa=iii",
+			true,
+		},
+		{
+			"valid request",
+			"hoge=fuga",
+			"foo=bar&aaa=iii",
+			false,
+		},
+	} {
+		u, _ := url.Parse(testserver.URL + "/foo?" + tt.query)
+		c := covercheck.NewRequestCheckerPOST(u, headers, []byte(tt.form))
 		checker := c.Checker()
-		if err := checker(context.Background()); err == nil {
-			t.Error("GET request should return error")
-		}
-	})
-
-	t.Run("POST request", func(t *testing.T) {
-		headers := http.Header{
-			"Content-Type": []string{
-				"application/x-www-form-urlencoded",
-			},
-		}
-		for _, tt := range []struct {
-			label       string
-			query       string
-			form        string
-			expectedErr bool
-		}{
-			{
-				"wrong query",
-				"hoge=fugo",
-				"foo=bar&aaa=iii",
-				true,
-			},
-			{
-				"wrong post body",
-				"hoge=fuga",
-				"aaa=iii",
-				true,
-			},
-			{
-				"valid request",
-				"hoge=fuga",
-				"foo=bar&aaa=iii",
-				false,
-			},
-		} {
-			u, _ := url.Parse(testserver.URL + "/foo?" + tt.query)
-			c := covercheck.NewRequestCheckerPOST(u, headers, []byte(tt.form))
-			checker := c.Checker()
-			{
-				actual := checker(context.Background()) != nil
-				if actual != tt.expectedErr {
-					t.Errorf("[%s] expected:%v, actual:%v", tt.label, tt.expectedErr, actual)
-				}
+		{
+			actual := checker(context.Background()) != nil
+			if actual != tt.expectedErr {
+				t.Errorf("[%s] expected:%v, actual:%v", tt.label, tt.expectedErr, actual)
 			}
-			{
-				actual := checker(context.Background()) != nil
-				if actual != tt.expectedErr {
-					t.Errorf("2nd [%s] expected:%v, actual:%v", tt.label, tt.expectedErr, actual)
-				}
-			}
-
 		}
-	})
+		{
+			actual := checker(context.Background()) != nil
+			if actual != tt.expectedErr {
+				t.Errorf("2nd [%s] expected:%v, actual:%v", tt.label, tt.expectedErr, actual)
+			}
+		}
+	}
 }
